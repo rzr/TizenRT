d8051285be92af60ee17b078b5cccf9bcb2e3775
Philippe Coval <philippe.coval@osg.samsung.com>
2018-01-29 11:37:45 +0100

    iotivity: Add support for ARTIK055S to iotivity_simpleserver
    Change-Id: I292ae9c3f1fc846e76e860e3dc04637d8a0c0885
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- apps/examples/iotivity_simpleserver/simpleServer.c
+++ apps/examples/iotivity_simpleserver/simpleServer.c
@@ -84,7 +84,7 @@
 
 #if defined(CONFIG_ENC28J60) || defined(CONFIG_WICED)
 #define NET_DEVNAME "en"
-#elif defined(CONFIG_ARCH_BOARD_SIDK_S5JT200) || defined(CONFIG_ARCH_BOARD_ARTIK053)
+#elif defined(CONFIG_ARCH_BOARD_SIDK_S5JT200) || defined(CONFIG_ARCH_BOARD_ARTIK053) || defined(CONFIG_ARCH_BOARD_ARTIK055S)
 #define NET_DEVNAME "wl"
 #else
 #error "undefined CONFIG_NET_<type>, check your .config"
